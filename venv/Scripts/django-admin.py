10425c6f2931d15f0bf16edde030c4e3619539cd
SaumoPal97 <saumopal1997@iitkgp.ac.in>
2020-06-06 10:44:55 +0530

    login system done

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USER\Desktop\Products and Solutions\EkAnek\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
